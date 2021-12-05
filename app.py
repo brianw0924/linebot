877b922590edd07d0e7ad04dc311c7110ca57778
brianw0924 <brianw0924@gmail.com>
2021-12-05 22:33:01 +0800

    add database

--- app.py
+++ app.py
@@ -28,13 +28,45 @@ def callback():
         abort(400)
     return 'OK'
 
+def insert_data(record):
+    DATABASE_URL = os.environ('DATABASE_URL')
+    
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+
+    table_columns = '(name, type)'
+    postgres_insert_query = f"""INSERT INTO food {table_columns} VALUES (%s,%s)"""
+
+    cursor.execute(postgres_insert_query, record)
+    conn.cimmit()
+
+    message = f"成功存入餐廳: {record[0]} | 類型: {record[1]}"
+
+    print(message)
+
+    cursor.close()
+    conn.close()
+
+    return message
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text) # reply message
-    line_bot_api.reply_message(event.reply_token, message) # send back
+
+    raw_msg = event.message.text
+    food_name = raw_msg.split(' ')[0]
+    type_name = raw_msg.split(' ')[1]
+    record = (food_name, type_name)
+    if type_name not in ['早餐','午餐','晚餐','飲料','點心']:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請輸入:[餐廳名字] [空格] [類型(早餐,午餐,晚餐,飲料,點心)]')) # send back
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=insert_data(record)) # send back
+
+    # message = TextSendMessage(text=event.message.text) # reply message
+    # line_bot_api.reply_message(event.reply_token, message) # send back
 
 import os
+import psycopg2
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
